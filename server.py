97e4be55e47ad083ae1d7a1cb1712dea50d63d67
Sparsh Agarwal <sparshcode@gmail.com>
2016-12-24 00:05:32 +0530

    error file working

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def error(e):
+	return render_template('404.html'), 404
+
 @app.route('/')
 def main():
 	return render_template("main.html")
